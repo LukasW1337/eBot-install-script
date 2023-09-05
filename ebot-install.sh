66e2c6e2a0a926d2395da91a8b666bbf850a98dc
LukasW1337 <33436545+LukasW1337@users.noreply.github.com>
2023-09-05 12:29:13 +0200

    Update ebot-install.sh

--- ebot-install.sh
+++ ebot-install.sh
@@ -164,7 +164,7 @@ else
 	echo 'date.timezone = Europe/Paris' >> /usr/local/lib/php.ini
 	echo 'extension=pthreads.so' >> /usr/local/lib/php.ini
 	
-	apt-get install libapache2-mod-php5.6 php5.6-curl -y
+	apt-get install libapache2-mod-php5.6 php5.6-curl php5.6-xml -y
 	if [ $? != 0 ]; then
 		echo "(LINE 162) There is an error. Are you running apt application somewhere?"
 		echo "Can you check your debian source list?"
